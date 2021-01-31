53b7d5138387641f59fae0507cfe8d465b92df91
William Chen <wchen298@gmail.com>
2021-02-01 08:18:42 +0900

    finish numeric dec test

--- terra_sdk/core/numeric.py
+++ terra_sdk/core/numeric.py
@@ -1,53 +1,97 @@
 from __future__ import annotations
 
 import re
+
 from decimal import Decimal
 from typing import Union
 
-SDK_DEC_PREC = 18
-SDK_DEC_UNIT = 10 ** SDK_DEC_PREC
-SDK_DEC_REGEX_PATTERN = r"^(\-)?(\d+)(\.\d+)?\Z"
-PATTERN = re.compile(SDK_DEC_REGEX_PATTERN)
+DEC_NUM_DIGITS = 18
+DEC_ONE = 10 ** DEC_NUM_DIGITS
+DEC_PATTERN = re.compile(r"^(\-)?(\d+)(\.(\d+))?\Z")
 
-__all__ = ["SDK_DEC_PREC", "Dec"]
+__all__ = ["DEC_NUM_DIGITS", "Dec"]
+
+
+def convert_to_dec_bignum(arg: Union[str, int, float, Decimal]):
+    if isinstance(arg, int):
+        return arg * DEC_ONE
+    if isinstance(arg, float):
+        arg = str("%f" % arg)
+    if isinstance(arg, str):
+        parts = DEC_PATTERN.match(arg)
+        if parts is None:
+            raise ValueError(f"Unable to parse Dec from string: {arg}")
+        result = int(parts.group(2)) * DEC_ONE  # whole part
+        if parts.group(3):
+            fraction = int(parts.group(4)[0:DEC_NUM_DIGITS].ljust(DEC_NUM_DIGITS, "0"))
+            result += fraction
+        if parts.group(1):
+            result *= -1
+        return result
+    elif isinstance(arg, Decimal):
+        whole = int(arg)
+        fraction = arg % 1
+        return int((whole * DEC_ONE) + (fraction * DEC_ONE))
+    else:
+        raise TypeError(
+            f"Unable to parse Dec integer representation from given argument {arg}"
+        )
+
+
+def chop_precision_and_round(d: int) -> int:
+    """Cosmos-SDK's banker's rounding: https://github.com/cosmos/cosmos-sdk/blob/1d75e0e984e7132efd54c3526e36b3585e2d91c0/types/decimal.go#L491"""
+    if d < 0:
+        return -1 * chop_precision_and_round(d * -1)
+
+    quo, rem = d // DEC_ONE, d % DEC_ONE
+
+    if rem == 0:
+        return quo
+
+    if rem < DEC_ONE / 2:
+        return quo
+    elif rem > DEC_ONE / 2:
+        return quo + 1
+    else:
+        if quo % 2 == 0:
+            return quo
+        return quo + 1
 
 
 class Dec:
-    def __init__(self, arg):
+
+    _i: int = None
+
+    def __init__(self, arg: Union[str, int, float, Decimal, Dec]):
         """BigInt-based Decimal representation with basic arithmetic operations with
         compatible Python numeric types (int, float, Decimal). Does not work with
-        NaN, Infinity, +0, -0, etc. Serializes as a string wi4th 18 points of decimal
-        precision.
+        NaN, Infinity, +0, -0, etc. Serializes as a string with 18 points of decimal
+        DEC_NUM_DIGITS.
         """
-        if isinstance(arg, float):
-            arg = str("%f" % arg)
-        if isinstance(arg, str):
-            parts = PATTERN.match(arg)
-            if parts is None:
-                raise ValueError(f"Unable to parse Dec object from string: {arg}")
-            self.i = int(parts.group(2)) * SDK_DEC_UNIT  # integer part
-            if parts.group(3):
-                fraction = (
-                    parts.group(3).lstrip(".")[0:SDK_DEC_PREC].ljust(SDK_DEC_PREC, "0")
-                )
-                self.i += int(fraction)
-            if parts.group(1) is not None:
-                self.i *= -1
-        elif isinstance(arg, int):
-            self.i = arg * SDK_DEC_UNIT
-        elif isinstance(arg, Dec):
-            self.i = arg.i
-        elif isinstance(arg, Decimal):
-            whole = int(arg)
-            fraction = arg % 1
-            self.i = (whole * SDK_DEC_UNIT) + (fraction * SDK_DEC_UNIT)
+        if isinstance(arg, Dec):
+            self._i = arg._i
+            return
         else:
-            raise TypeError(f"Unable to create Dec object from given argument {arg}")
-        # guarantee int
-        self.i = int(self.i)
+            self._i = int(convert_to_dec_bignum(arg))
+
+    @classmethod
+    def zero(cls):
+        return cls(0)
+
+    @classmethod
+    def one(cls):
+        nd = cls(0)
+        nd._i = DEC_ONE
+        return nd
+
+    def __str__(self) -> str:
+        if self._i == 0:
+            return "0." + DEC_NUM_DIGITS * "0"
+        parity = "-" if self._i < 0 else ""
+        return f"{parity}{self.whole}.{self.frac}"
 
     def to_short_str(self):
-        parity = "-" if self.i < 0 else ""
+        parity = "-" if self._i < 0 else ""
         frac = self.frac.rstrip("0")
         dot = "." if len(frac) > 0 else ""
         return f"{parity}{self.whole}{dot}{frac}"
@@ -55,129 +99,127 @@ class Dec:
     def __repr__(self):
         return f"Dec({self.to_short_str()})"  # short representation
 
-    def __str__(self) -> str:
-        if self.i == 0:
-            return "0." + SDK_DEC_PREC * "0"
-        parity = "-" if self.i < 0 else ""
-        return f"{parity}{self.whole}.{self.frac}"
-
     def __int__(self) -> int:
-        int_part = abs(self.i) // SDK_DEC_UNIT
-        int_part *= -1 if self.i < 0 else 1
+        int_part = abs(self._i) // DEC_ONE
+        int_part *= -1 if self._i < 0 else 1
         return int_part
 
     def __float__(self) -> float:
         # NOTE: This is not robust enough for: float(Dec(float)) to give the same output,
         # and should mainly be used as getting a rough value from the Dec object.
-        return self.i / SDK_DEC_UNIT
+        return float(self._i) / DEC_ONE
 
     @property
     def parity(self) -> int:
-        return -1 if self.i < 0 else 1
+        return -1 if self._i < 0 else 1
 
     @property
     def whole(self) -> str:
-        return str(abs(self.i) // SDK_DEC_UNIT)
+        return str(abs(self._i) // DEC_ONE)
 
     @property
     def frac(self) -> str:
-        return str(abs(self.i) % SDK_DEC_UNIT).rjust(SDK_DEC_PREC, "0")
+        return str(abs(self._i) % DEC_ONE).rjust(DEC_NUM_DIGITS, "0")
 
     def to_data(self) -> str:
         return str(self)
 
-    def _pretty_repr_(self, path: str = ""):
-        return self.short_str
-
     def __eq__(self, other) -> bool:
-        try:
-            return self._i_binop(other, int.__eq__, "==")
-        except TypeError:
+        if isinstance(other, str):
             return False
+        else:
+            return self._i == Dec(other)._i
+
+    def lt(self, other) -> bool:
+        if isinstance(other, Dec):
+            return self._i < other._i
+        return (Decimal(self._i) / DEC_ONE) < other
 
     def __lt__(self, other) -> bool:
-        if isinstance(other, Dec):
-            return self.i < other.i
-        return (Decimal(self.i) / SDK_DEC_UNIT) < other
+        return self.lt(other)
+
+    def le(self, other) -> bool:
+        return self < other or self.__eq__(other)
 
     def __le__(self, other) -> bool:
-        return self < other or self == other
+        return self.le(other)
+
+    def gt(self, other) -> bool:
+        if isinstance(other, Dec):
+            return self._i > other._i
+        return (Decimal(self._i) / DEC_ONE) > other
 
     def __gt__(self, other) -> bool:
-        if isinstance(other, Dec):
-            return self.i > other.i
-        return (Decimal(self.i) / SDK_DEC_UNIT) > other
+        return self.gt(other)
+
+    def ge(self, other) -> bool:
+        return self.gt(other) or self.__eq__(other)
 
     def __ge__(self, other) -> bool:
-        return self > other or self == other
+        return self.ge(other)
+
+    def add(self, other) -> Dec:
+        nd = Dec.zero()
+        nd._i = self._i + Dec(other)._i
+        return nd
 
     def __add__(self, other) -> Dec:
-        new_val = self._i_binop(other, int.__add__, "+")
-        nd = Dec(0)
-        nd.i = new_val
-        return nd
+        return self.add(other)
 
     def __radd__(self, other):
-        return Dec.__add__(Dec(other), self)
+        return Dec(other).add(self)
+
+    def sub(self, other) -> Dec:
+        nd = Dec.zero()
+        nd._i = self._i - Dec(other)._i
+        return nd
 
     def __sub__(self, other) -> Dec:
-        new_val = self._i_binop(other, int.__sub__, "-")
-        nd = Dec(0)
-        nd.i = new_val
-        return nd
+        return self.sub(other)
 
     def __rsub__(self, other):
-        return Dec.__sub__(Dec(other), self)
+        return Dec(other).sub(self)
 
-    def __mul__(self, other):
-        if isinstance(other, Dec):
-            new_val = self.i * other.i
-        elif (
-            isinstance(other, int)
-            or isinstance(other, float)
-            or isinstance(other, Decimal)
-        ):
-            new_val = self.i * Dec(other).i
-        else:
-            raise TypeError(
-                f"unsupported operand types for *: 'Dec' and '{type(other)}'"
-            )
-        nd = Dec(0)
-        nd.i = new_val // SDK_DEC_UNIT
+    def mul(self, other: Union[str, int, float, Decimal, Dec]) -> Dec:
+        x = self._i
+        y = Dec(other)._i
+        nd = Dec.zero()
+        nd._i = chop_precision_and_round(x * y)
         return nd
+
+    def __mul__(self, other) -> Dec:
+        return self.mul(other)
 
     def __rmul__(self, other):
-        return Dec.__mul__(Dec(other), self)
+        return Dec(other).mul(self)
 
-    def __truediv__(self, other):
-        try:
-            # we need the decimal value's better precision.
-            d1 = Decimal(self.i)
-            d2 = Decimal(other.i)
-            new_val = d1 / d2
-        except AttributeError:
-            new_val = self._i_binop(other, int.__truediv__, "/")
-        nd = Dec(0)
-        nd.i = int(new_val * SDK_DEC_UNIT)
+    def div(self, other) -> Dec:
+        if Dec(other)._i == 0:
+            raise ZeroDivisionError(f"tried to divide by 0: {self!r} / {other!r}")
+        nd = Dec.zero()
+        nd._i = chop_precision_and_round(self._i * DEC_ONE * DEC_ONE // Dec(other)._i)
         return nd
 
-    def __rtruediv__(self, other):
-        return Dec.__truediv__(Dec(other), self)
+    def __truediv__(self, other) -> Dec:
+        return self.div(other)
+
+    def __rtruediv__(self, other) -> Dec:
+        return Dec(other).div(self)
 
     def __floordiv__(self, other):
-        return Dec(int(self / Dec(other)))
+        return self.div(int(other))
 
-    def __neg__(self):
+    def __neg__(self) -> Dec:
         x = Dec(self)
-        x.i *= -1
+        x._i *= -1
         return x
 
-    def __abs__(self):
+    def __abs__(self) -> Dec:
         x = Dec(self)
-        x.i = abs(x.i)
+        x._i = abs(x.i)
         return x
 
-    def __pos__(self):
+    def __pos__(self) -> Dec:
         return abs(self)
 
     @classmethod
@@ -186,34 +228,16 @@ class Dec:
 
     @classmethod
     def with_prec(cls, i: int, prec: int) -> Dec:
-        """Replicates Dec.with_prec(i, prec)"""
+        """Replicates Cosmos-SDK's Dec.with_prec(i, prec)"""
         d = cls(0)
         i = int(i)
-        d.i = i * 10 ** (SDK_DEC_PREC - int(prec))
+        d._i = i * 10 ** (DEC_NUM_DIGITS - int(prec))
         return d
-
-    def _i_binop(self, other, binop, binop_name):
-        """Helper method that tries to work with compatible number types by first converting
-        them into Dec and working with their internal BigInt representation.
-        """
-        if isinstance(other, Dec):
-            new_val = binop(self.i, other.i)
-        elif (
-            isinstance(other, int)
-            or isinstance(other, float)
-            or isinstance(other, Decimal)
-        ):
-            new_val = binop(self.i, Dec(other).i)
-        else:
-            raise TypeError(
-                f"unsupported operand types for {binop_name}: 'Dec' and '{type(other)}'"
-            )
-        return new_val
 
 
 class Numeric:
 
-    Input = Union[str, int, float, Dec, Decimal]
+    Input = Union[str, int, float, Decimal, Dec]
     Output = Union[int, Dec]
 
     @staticmethod
