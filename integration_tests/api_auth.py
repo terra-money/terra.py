5ca21a8923126e4f6442b69709f7459e7771b0f3
brycepratt <bbsrpratt@gmail.com>
2021-05-27 17:39:04 +0900

    Formatting commit

--- integration_tests/api_auth.py
+++ integration_tests/api_auth.py
@@ -6,14 +6,14 @@ base_account_address = "terra1fa0trn2nqjc2n6mmz9txta7ky5h5nnp9m6cra3"
 base_account_data = {
     "type": "core/Account",
     "value": {
-      "address": "terra1fa0trn2nqjc2n6mmz9txta7ky5h5nnp9m6cra3",
-      "public_key": {
-        "type": "tendermint/PubKeySecp256k1",
-        "value": "A6/5zM6Vo11e3aepClYLVn4rsHdXOnFsDccXYiuvSeeJ"
-      },
-      "account_number": "27",
-      "sequence": "0"
-    }
+        "address": "terra1fa0trn2nqjc2n6mmz9txta7ky5h5nnp9m6cra3",
+        "public_key": {
+            "type": "tendermint/PubKeySecp256k1",
+            "value": "A6/5zM6Vo11e3aepClYLVn4rsHdXOnFsDccXYiuvSeeJ",
+        },
+        "account_number": "27",
+        "sequence": "0",
+    },
 }
 base_account = bombay.auth.account_info(base_account_address)
 base_account.sequence = 0  # Set sequence # to 0 since it's dynamic
