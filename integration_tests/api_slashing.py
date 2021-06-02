78b5ec41e90a38a937ac060608e75a70102e5116
brycepratt <bbsrpratt@gmail.com>
2021-06-02 15:12:31 +0900

    Added coverage to slashing endpoints

--- integration_tests/api_slashing.py
+++ integration_tests/api_slashing.py
@@ -8,3 +8,6 @@ assert "min_signed_per_window" in parameters
 assert "downtime_jail_duration" in parameters
 assert "slash_fraction_double_sign" in parameters
 assert "slash_fraction_downtime" in parameters
+
+signing_infos = bombay.slashing.signing_infos()
+assert type(signing_infos[0]) == dict if len(signing_infos) > 0 else True
