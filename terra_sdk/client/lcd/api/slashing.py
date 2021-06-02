f43f3fe8e3d0b74474ec0146ad79c3648ec06260
brycepratt <bbsrpratt@gmail.com>
2021-06-02 14:17:50 +0900

    Removed support for filtering signing_infos by validator

--- terra_sdk/client/lcd/api/slashing.py
+++ terra_sdk/client/lcd/api/slashing.py
@@ -1,6 +1,4 @@
-from typing import List, Optional, Union
-
-from terra_sdk.core import ValConsPubKey
+from typing import List
 
 from ._base import BaseAsyncAPI, sync_bind
 
@@ -8,22 +6,13 @@ __all__ = ["AsyncSlashingAPI", "SlashingAPI"]
 
 
 class AsyncSlashingAPI(BaseAsyncAPI):
-    async def signing_infos(
-        self, val_cons_pub_key: Optional[ValConsPubKey] = None
-    ) -> Union[List[dict], dict]:
-        """Fetches signing infos, optionally filtering by validator consensus public key.
-
-        Args:
-            val_cons_pub_key (Optional[ValConsPubKey], optional): validator consensus public key.
+    async def signing_infos(self) -> List[dict]:
+        """Fetches signing infos.
 
         Returns:
-            Union[List[dict], dict]: signing infos
+            List[dict]: signing infos
         """
-        if val_cons_pub_key is None:
-            url = "/slashing/signing_infos"
-        else:
-            url = f"/slashing/validators/{val_cons_pub_key}/signing_info"
-        return await self._c._get(url)
+        return await self._c._get("/slashing/signing_infos")
 
     async def parameters(self) -> dict:
         """Fetches Slashing module parameters.
@@ -36,9 +25,7 @@ class AsyncSlashingAPI(BaseAsyncAPI):
 
 class SlashingAPI(AsyncSlashingAPI):
     @sync_bind(AsyncSlashingAPI.signing_infos)
-    def signing_infos(
-        self, val_cons_pub_key: Optional[ValConsPubKey] = None
-    ) -> List[dict]:
+    def signing_infos(self) -> List[dict]:
         pass
 
     signing_infos.__doc__ = AsyncSlashingAPI.signing_infos.__doc__
