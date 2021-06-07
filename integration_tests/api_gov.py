72e59b648acce1ece4eee473c3a02056da2ad71e
brycepratt <bbsrpratt@gmail.com>
2021-06-07 19:04:53 +0900

    Added coverage to wasm and gov and left comments to add more tests once genesis improved

--- integration_tests/api_gov.py
+++ integration_tests/api_gov.py
@@ -1,4 +1,5 @@
 from terra_sdk.client.lcd import LCDClient
+from terra_sdk.core.gov import Proposal
 
 bombay = LCDClient(url="https://bombay-lcd.terra.dev", chain_id="bombay-0001")
 
@@ -12,3 +13,16 @@ assert "tally_params" in parameters
 assert "quorum" in parameters["tally_params"]
 assert "threshold" in parameters["tally_params"]
 assert "veto_threshold" in parameters["tally_params"]
+
+proposal_id = "14"
+
+proposal = bombay.gov.proposal(proposal_id)
+assert type(proposal) == Proposal
+
+# TODO: Add proposal data to genesis and then add rest of coverage cases
+
+# proposer = bombay.gov.proposer(proposal_id)
+# assert type(proposer) == str
+
+# desposits = bombay.gov.deposits(proposal_id)
+# assert desposits is not None
--- integration_tests/api_wasm.py
+++ integration_tests/api_wasm.py
@@ -8,3 +8,9 @@ assert "max_contract_gas" in parameters
 assert "max_contract_msg_size" in parameters
 assert "max_contract_data_size" in parameters
 assert "event_params" in parameters
+
+code_info = bombay.wasm.code_info(1)
+assert "code_id" in code_info
+assert "creator" in code_info
+
+# TODO: Add tests for contract_info and contract_query (require contracts included in genesis)
