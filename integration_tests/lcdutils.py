bc4597ec3509affd08e6d15505f8b27c86710e20
Geoff Lee <geoff@terra.money>
2021-10-10 17:26:46 +0900

    update chain-id

--- integration_tests/lcdutils.py
+++ integration_tests/lcdutils.py
@@ -1,6 +1,6 @@
 from terra_sdk.client.lcd import LCDClient
 
-terra = LCDClient(chain_id="columbus-4", url="https://lcd.terra.dev")
+terra = LCDClient(chain_id="columbus-5", url="https://lcd.terra.dev")
 validators_with_voting_power = terra.utils.validators_with_voting_power()
 print(validators_with_voting_power)
 tax = terra.utils.calculate_tax("100000000uusd")
