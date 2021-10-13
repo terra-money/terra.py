643c086616d812427670db5f160e5bc3da9bdf6d
Geoff Lee <geoff@terra.money>
2021-10-13 10:17:33 +0900

    rstrip for compatibility

--- terra_sdk/util/url.py
+++ terra_sdk/util/url.py
@@ -2,4 +2,4 @@ from furl import furl  # type: ignore
 
 
 def urljoin(base: str, url: str) -> str:
-    return furl(base.removesuffix("/")).add(path=url).url
+    return furl(base.rstrip("/")).add(path=url).url
