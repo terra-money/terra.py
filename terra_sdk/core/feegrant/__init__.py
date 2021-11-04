cb3b56c6574a4273b993c01f91f371b6404ff53e
Geoff Lee <geoff@terra.money>
2021-11-04 17:19:27 +0900

    add feegrant module for col5/proto

---
+++ terra_sdk/core/feegrant/__init__.py
@@ -0,0 +1,10 @@
+from .data import BasicAllowance, PeriodicAllowance, AllowedMsgAllowance
+from .msgs import MsgGrantAllowance, MsgRevokeAllowance
+
+__all__ = [
+    "BasicAllowance",
+    "PeriodicAllowance",
+    "AllowedMsgAllowance",
+    "MsgGrantAllowance",
+    "MsgRevokeAllowance"
+]
