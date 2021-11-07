bbdd04753616f675fb1f74f69a219ce2cb16484f
Geoff Lee <geoff@terra.money>
2021-11-07 16:37:42 +0900

    update CompactBitArray

--- terra_sdk/core/tx.py
+++ terra_sdk/core/tx.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import base64
+import math
 from typing import Dict, List, Optional
 
 import attr
@@ -41,6 +42,7 @@ __all__ = [
     "ModeInfoMulti",
     "SignerInfo",
     "SignerData",
+    "CompactBitArray"
 ]
 
 # just alias
@@ -341,7 +343,7 @@ class ModeInfoMulti(JSONSerializable):
 @attr.s
 class CompactBitArray(JSONSerializable):
     extra_bits_stored: int = attr.ib(converter=int)
-    elems: str = attr.ib()
+    elems: bytes = attr.ib()
 
     @classmethod
     def from_data(cls, data: dict) -> CompactBitArray:
@@ -351,9 +353,57 @@ class CompactBitArray(JSONSerializable):
     def from_proto(cls, proto: CompactBitArray_pb) -> CompactBitArray:
         return cls(proto["extra_bits_stored"], proto["elems"])
 
+    def to_proto(self) -> CompactBitArray_pb:
+        return CompactBitArray_pb(
+            extra_bits_stored=self.extra_bits_stored,
+            elems=self.elems
+        )
+
     @classmethod
-    def from_bits(cls, len: int) -> CompactBitArray:
-        raise NotImplementedError  # TODO
+    def from_bits(cls, bits: int) -> CompactBitArray:
+        if bits <= 0:
+            raise ValueError("CompactBitArray bits must be bigger than 0")
+
+        num_elems = (bits+7)/8
+        if num_elems <= 0 or num_elems > (math.pow(2, 32)-1):
+            raise ValueError("CompactBitArray overflow")
+
+        return CompactBitArray(bits%8, bytes(num_elems))
+
+    def count(self) -> int:
+        if self.extra_bits_stored == 0:
+            return len(self.elems)*8
+        return (len(self.elems)-1)*8 + self.extra_bits_stored
+
+    def get_index(self, i: int) -> bool:
+        if i < 0 or i >= self.count():
+            return False
+        return self.elems[(i>>3)] & ( (1 << (7 - (i % 8))) > 0 )
+
+    def set_index(self, i: int, v: bool) -> bool:
+        if i < 0 or i >= self.count():
+            return False
+        if v:  # True
+            self.elems[i >> 3] |= 1 << (7-(i%8))
+        else:  # False
+            self.elems[i >> 3] &= ~(1 << (7-(i%8)))
+        return True
+
+    def num_true_bits_before(self, index: int) -> int:
+        def count_one_bits(n: int):
+            return len("{0:b}".format(n).split("0").join(''))
+
+        ones_count = 0
+        max = self.count()
+        if index > max:
+            index=max
+
+        elem = 0
+        while True:
+            if elem*8+7 >= index:
+                ones_count += count_one_bits(self.elems[elem] >> (7-(index%8)+1))
+                return ones_count
+            ones_count += count_one_bits(self.elems[elem])
 
 
 def parse_events_by_type(event_data: List[dict]) -> Dict[str, Dict[str, List[str]]]:
