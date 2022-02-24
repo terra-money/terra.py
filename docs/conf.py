211a616580da966630fe2d5839ab2f5fa9ed10c1
Geoff Lee <geoff@terra.money>
2022-02-24 15:14:30 +0900

    revert theme to rtd

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../"))
 
-import sphinx_book_theme
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -38,7 +38,7 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.autodoc",
     "sphinx_autodoc_typehints",
-    "sphinx_book_theme",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,7 +55,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
