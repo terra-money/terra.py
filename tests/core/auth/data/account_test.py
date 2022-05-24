7ba314e33e17ae5dd74b669ac5bd5751b0f3eac3
JSHan94 <saw1515@postech.ac.kr>
2022-05-24 15:17:24 +0900

    add testcases for core/auth

--- tests/core/auth/data/account_test.py
+++ tests/core/auth/data/account_test.py
@@ -1,4 +1,4 @@
-from terra_sdk.core.auth import Account, LazyGradedVestingAccount
+from terra_sdk.core.auth import Account, DelayedVestingAccount, ContinuousVestingAccount, PeriodicVestingAccount
 
 
 def test_deserializes_account_example(load_json_examples):
@@ -6,8 +6,17 @@ def test_deserializes_account_example(load_json_examples):
     for example in examples:
         assert Account.from_amino(example).to_amino() == example
 
-
-def test_deserializes_lazy_graded_vesting_account_example(load_json_examples):
-    examples = load_json_examples("./LazyGradedVestingAccount.data.json")
+def test_deserializes_continuous_vesting_account_example(load_json_examples):
+    examples = load_json_examples("./ContinuousVestingAccount.data.json")
     for example in examples:
-        assert LazyGradedVestingAccount.from_amino(example).to_amino() == example
+        assert ContinuousVestingAccount.from_amino(example).to_amino() == example
+
+def test_deserializes_delayed_vesting_account_example(load_json_examples):
+    examples = load_json_examples("./DelayedVestingAccount.data.json")
+    for example in examples:
+        assert DelayedVestingAccount.from_amino(example).to_amino() == example
+
+def test_deserializes_periodic_vesting_account_example(load_json_examples):
+    examples = load_json_examples("./PeriodicVestingAccount.data.json")
+    for example in examples:
+        assert PeriodicVestingAccount.from_amino(example).to_amino() == example
